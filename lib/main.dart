ecd810091eae2e57033082bc198ab699bda0890c
robotics <admin@robotics>
2024-02-20 07:42:53 +0300

    build

--- lib/main.dart
+++ lib/main.dart
@@ -9,12 +9,68 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
-    );
+    return const MaterialApp(home: HomeApp());
+  }
+}
+
+class HomeApp extends StatefulWidget {
+  const HomeApp({super.key});
+
+  @override
+  State<HomeApp> createState() => _HomeAppState();
+}
+
+class _HomeAppState extends State<HomeApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: const Color(0xFF1C2757),
+        body: SafeArea(
+          child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Center(
+                    child: Text("Until the end of assignment",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 28.0,
+                          fontWeight: FontWeight.bold,
+                        )),
+                  ),
+                  SizedBox(
+                    height: 20.0,
+                  ),
+                  Container(
+                    height: 400.0,
+                    decoration: BoxDecoration(
+                      color: Color(0xFF323F68),
+                      borderRadius: BorderRadius.circular(8.0),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20.0,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Expanded(
+                        child: RawMaterialButton(
+                          onPressed: () {},
+                          shape: const StadiumBorder(
+                              side: BorderSide(color: Colors.blue)),
+                          child: Text(
+                            "start",
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ),
+                      )
+                    ],
+                  )
+                ],
+              )),
+        ));
   }
 }
